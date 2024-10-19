9d8c7f61d18450f8f4d910466dedc4be8e4a75b5
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2024-10-19 11:34:54 +0700

    Create AbstractParent.java

---
+++ Abstract Class & Interfaces/AbstractClass/AbstractParent.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.oop.abstractparent;
+
+/**
+ *
+ * @author bintang
+ */
+public class AbstractParent {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+                        
+        System.out.println(child.abstractMethod());
+    }
+}
